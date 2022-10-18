29c4e8ba828345cf23d242f1aac48d6a82a17a46
Brooke Ross <brooke.ross2800@gmail.com>
2022-10-18 10:30:58 -0600

    second finish for flow

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,18 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
+
+    # creates a freebie but not quite sure how to test:
+    
+    def give_freebie(dev,item_name,value)
+        Freebie.create(item_name: item_name, value: value, dev_id:dev, company_id:self)
+    end
+
+    # searches for oldest company by selecting and ordering them by founding year, then returning the first response:
+
+    def self.oldest_company
+        Company.all.order(:founding_year).first
+    end
+
 
 end
