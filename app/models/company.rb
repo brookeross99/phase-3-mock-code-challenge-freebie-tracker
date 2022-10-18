959c8889f631ef96d3c69e6c50f6ecba924310ee
Brooke Ross <brooke.ross2800@gmail.com>
2022-10-18 10:04:57 -0600

    finished first time

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,17 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
+
+    # Creates a new freebie associated with the dev, item name and value that is put in. Company is self right now. 
+    
+    def give_freebie(dev,item_name,value)
+        Freebie.create(item_name: item_name, value: value, dev_id:dev , company:self)
+    end
+
+    # Puts all of the companies in order of founding year and returns the first search:
+
+    def self.oldest_company
+        Company.all.order(:founding_year).first
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+mifflin = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+meseeks = Dev.create(name: "Mr. Meseeks")
+gazor = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,14 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+
+Freebie.create(item_name:"free ticket that can be reedeemed 1 time: the federation of ricks will not interfer for 24 hrs.",value: 200000000000, dev_id:rick.id, company_id:google.id)
+Freebie.create(item_name:"jessica mug",value: 5, dev_id:morty.id, company_id:facebook.id)
+Freebie.create(item_name:"paper",value: 35, dev_id:meseeks.id, company_id:mifflin.id)
+Freebie.create(item_name:"pickle",value: 99, dev_id:gazor.id, company_id:enron.id)
+Freebie.create(item_name:"multi dementional travel",value: 200, dev_id:rick.id, company_id:facebook.id)
+Freebie.create(item_name:"backpack",value: 4, dev_id:meseeks.id, company_id:mifflin.id)
+Freebie.create(item_name:"clone gun",value: 50, dev_id:rick.id, company_id:facebook.id)
+
 
 puts "Seeding done!"
