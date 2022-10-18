4f1a17d77fab2dc5ebffe4d6774f18b3e9c7b285
Brooke Ross <brooke.ross2800@gmail.com>
2022-10-18 11:58:06 -0600

    finished with Matt and wrote notes about how to test methods!

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -11,6 +11,25 @@ class Dev < ActiveRecord::Base
     end
 
     def give_away(freebie,dev)
-        freebie.update(dev: dev) unless freebie.dev != self
+        # freebie.update(dev: dev) freebie.dev != dev.id
+        # self refrences the Dev who is giving the freebie away
+        # freebie = the instance that we are about to modify the Dev to.
+        # dev is an instance that refrences the dev that is receiving the freebie. 
+
+        # check if the freebie belongs to self
+        # check if freebie.dev matches self
+
+        if freebie.dev == self
+            freebie.update(dev: dev)
+        else 
+            "This item is not owned by #{self.name}"
+        end
+        
     end
+    # To test: first make sure you can access your Dev.all and Freebie.all
+        # first=Dev.first -- save your instance to a variable.
+        # free1=Freebie.first --save your instance of a freebie to a variable as well
+        # dev2=Dev.second -- save the Dev instance we want to not change
+        # first.give_away(free1,dev2)
+        # Should change the above from Rick to the Morty Dev. 
 end
